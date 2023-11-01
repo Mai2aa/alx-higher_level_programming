3d5e65b8420e089076d2d75a2640201348c1a16f
Mai2aa <mysaadil0@gmail.com>
2023-11-01 02:08:23 +0200

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,5 +8,6 @@ if digit > 5:
 elif digit == 0:
     print("{} {} is {} and is 0".format(str, number, digit))
 elif digit < 6 and digit != 0:
-    print("{} {} is {} and is less than 6" \
+    print(
+        "{} {} is {} and is less than 6"
         " and not 0".format(str, number, digit))
