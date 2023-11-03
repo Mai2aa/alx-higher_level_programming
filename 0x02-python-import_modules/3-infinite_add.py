30a95aac79d08152bfb3912df14cf5351fc67345
Mai2aa <mysaadil0@gmail.com>
2023-11-03 03:43:08 +0200

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 if __name__ == '__main__':
     import sys
     import math
     result = 0
-    for i in sys.argv:
+    for i in sys.argv[1:]:
         result = result + int(i)
-        print("{}".format(result))
+    print("{}".format(result))
