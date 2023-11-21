322d9b5d64e90dd791791d2a3a90bd8e06c8bd94
Mai2aa <mysaadil0@gmail.com>
2023-11-21 14:10:45 +0200

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/pyhton3
+""" size validation"""
+
+
+class Square:
+    """ define a calss"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
