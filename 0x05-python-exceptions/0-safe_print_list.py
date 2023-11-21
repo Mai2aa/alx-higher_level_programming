094ec613dee138b31c542672d42b45be3e613e1d
Mai2aa <mysaadil0@gmail.com>
2023-11-21 03:07:17 +0200

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             count += 1
             if count == x:
                 break
-            print()
+        print()
     except TypeError:
-        return count
+        return 
     return count
