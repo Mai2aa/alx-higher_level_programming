978744236caadb1e77dd98c82b0e4cf17dc92416
Mai2aa <mysaadil0@gmail.com>
2023-11-21 16:13:29 +0200

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,5 +40,5 @@ class Square:
         else:
             for i in range(self.__position[1]):
                 print()
-            for i in range(self.__size):
+            for j in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
