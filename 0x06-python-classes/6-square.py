f4e82705044d00a79b75811f22e22996afc098f3
Mai2aa <mysaadil0@gmail.com>
2023-11-21 18:13:02 +0200

    task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,10 +36,10 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        if self.size == 0:
+        if self.__size == 0:
             print()
         else:
-            for i in range(self.position[1]):
+            for i in range(self.__position[1]):
                 print()
-            for j in range(self.size):
-                print(" " * self.position[0] + "#" * self.size)
+            for j in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
