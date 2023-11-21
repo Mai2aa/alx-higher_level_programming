247584f61ac71115564de85923d59ee986cf7cd2
Mai2aa <mysaadil0@gmail.com>
2023-11-21 03:54:26 +0200

    task 3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         count = 0
         for i in my_list:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(my_list[count]), end="")
             count += 1
             if count == x:
                 break
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,10 +4,12 @@ def safe_print_list_integers(my_list=[], x=0):
         count = 0
         for i in my_list:
             if isinstance(i, int):
-                print("{:d}".format(i), end="")
+                print("{:d}".format(my_list[count]), end="")
                 count += 1
                 if count == x:
                     break
+            else:
+                continue
         print()
     except (TypeError):
         return
