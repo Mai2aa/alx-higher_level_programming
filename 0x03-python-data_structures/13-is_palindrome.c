aa5356acbac9d829f83fd468dfcba4b560fa78cd
Mai2aa <mysaadil0@gmail.com>
2023-11-24 19:01:39 -0800

    task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,38 +8,26 @@
 */
 int is_palindrome(listint_t **head)
 {
-    listint_t *fast = *head, *slow = *head, *prev = NULL,
-    *next;
-
-    if (*head == NULL || (*head)->next == NULL) {
-        return (1);  // Empty list or list with only one element is considered a palindrome
-    }
-
-    while (fast != NULL && fast->next != NULL) {
-        fast = fast->next->next;
-
-        // Reverse the first half of the linked list while traversing
-        next = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = next;
-    }
-
-    // If the list has an odd number of nodes, move the slow pointer one step further
-    if (fast != NULL)
-    {
-        slow = slow->next;
-    }
-
-    // Compare the first half and second half of the linked list
-    while (prev != NULL && slow != NULL)
-    {
-        if (prev->n != slow->n) {
-            return (0);  // Not a palindrome
-        }
-        prev = prev->next;
-        slow = slow->next;
-    }
-
-    return (1);  // Palindrome
+	listint_t *fast = *head, *slow = *head, *prev = NULL,
+		  *next;
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		next = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = next;
+	}
+	if (fast != NULL)
+		slow = slow->next;
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+			return (0);
+		prev = prev->next;
+		slow = slow->next;
+	}
+	return (1);
 }
