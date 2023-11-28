9efec0be3a29a9532522a2313bdd3a9d26c725f2
Mai2aa <mysaadil0@gmail.com>
2023-11-28 04:24:15 +0200

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -61,12 +61,12 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    def bigger_or_equal(self, rect1, rect2):
-        if not isinstance(rect1, Rectangle):
-            raise TypeError("rect1 must be an instance of Rectangle")
-        if not isinstance(rect2, Rectangle):
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
             raise TypeError("rect2 must be an instance of Rectangle")
-        if rect1.area() >= rect2.area():
-            return rect1
+        if rect_1.area() >= rect_2.area():
+            return rect_1
         else:
-            return rect2
+            return rect_2
