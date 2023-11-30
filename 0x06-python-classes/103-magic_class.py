d8added5c78c6dfdca014c03d6ca831055dec383
Mai2aa <mysaadil0@gmail.com>
2023-11-30 23:13:13 +0200

    advanced

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import math
+"""ByteCode -> Python"""
+
+
+
+class MagicClass:
+    """define a class"""
+    def __init__(self, radius=0):
+        if not isinstance(radius, int) or not isinstance(radius, float):
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+    
+    def area(self):
+        return (self.__radius ** 2) * math.pi
+    
+    def circumference(self):
+        return math.pi * 2 * self.__radius
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-start of sprint 2 and a new journey with python 
+start of sprint 2
