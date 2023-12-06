2a747b2edbace8d2ecf5280b62780946c32b6c60
Mai2aa <mysaadil0@gmail.com>
2023-12-06 23:09:59 +0200

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-
+"""Write to a file"""
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as file:
+        file_n = file.write(text)
+    return file_n
