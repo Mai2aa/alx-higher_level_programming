ec6127765f451d0c103b9662e10d323d021fbe66
Mai2aa <mysaadil0@gmail.com>
2023-12-07 02:39:30 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,7 @@ from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
+"""this is a script that adds all arguments to python list"""
 items = load_from_json_file('add_item.json') or []
 items.extend(sys.argv[1:])
 save_to_json_file(items, 'add_item.json')
