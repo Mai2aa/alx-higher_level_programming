5bd7e422aa57bf7d1fd54a951588ed697058a46e
Mai2aa <mysaadil0@gmail.com>
2023-12-07 02:57:14 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 """this is a script that adds all arguments to python list"""
-
-
 import sys
 from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
