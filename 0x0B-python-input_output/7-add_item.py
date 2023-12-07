e49f9c9d91a6dfd9ad32e8978d77531b591e5e72
Mai2aa <mysaadil0@gmail.com>
2023-12-07 02:33:53 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""load, add, save"""
+
+
+import sys
+import json
+
+
+with open('add_item.json', 'r', encoding='utf-8') as file:
+    x = json.load(file)
+
+x.extend(sys.argv[1:])
+
+with open('add_item.json', 'w', encoding='utf-8') as file:
+    json.dump(x, file)
