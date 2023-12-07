fb5563a9c8c2d63aa6c28821b57c15c3923c3288
Mai2aa <mysaadil0@gmail.com>
2023-12-07 03:09:40 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
         __import__('6-load_from_json_file').load_from_json_file
 
     items = load_from_json_file("add_item.json") or []
-    items.extend(sys.argv[1:])
+    items.extend(list(sys.argv[1:]))
     save_to_json_file(items, "add_item.json")
