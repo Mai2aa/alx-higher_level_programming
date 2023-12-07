83b0664c0697019305b0168f7dc573759f947b54
Mai2aa <mysaadil0@gmail.com>
2023-12-07 02:51:52 +0200

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""load, add, save"""
+"""this is a script that adds all arguments to python list"""
 
 
 import sys
@@ -7,7 +7,6 @@ from save_to_json_file import save_to_json_file
 from load_from_json_file import load_from_json_file
 
 
-"""this is a script that adds all arguments to python list"""
 items = load_from_json_file('add_item.json') or []
 items.extend(sys.argv[1:])
 save_to_json_file(items, 'add_item.json')
