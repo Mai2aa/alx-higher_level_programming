8eae7386d469dd3ac265725e40bd5a3e72ecfa88
Mai2aa <mysaadil0@gmail.com>
2023-12-08 01:18:56 +0200

    tasks

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,16 @@
-#!/usr/bin/python2
+#!/usr/bin/python3
+"""print square"""
+
+
+def print_square(size):
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    elif not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for i in range(size):
+            for j in range(size):
+                print("#", end="")
+            print()
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""Text indentation"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    characters = ['.', '?', ':']
+    new = ''
+    result = ''
+    for char in text:
+        new += char
+        if char in characters:
+            result += new + "\n\n"
+            new = ''
+    if new:
+        result += new
+    print(result)
