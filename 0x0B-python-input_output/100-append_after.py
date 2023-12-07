7c56897a62848684b70f8ab7d5eec186cdfd07f2
Mai2aa <mysaadil0@gmail.com>
2023-12-07 04:30:10 +0200

    advanced

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""search and update """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """inserts a line of text to a file"""
+    with open(filename, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
