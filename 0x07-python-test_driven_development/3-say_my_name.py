86fdb09bbbff55e4e050ef1357de64c2be4fb9db
Mai2aa <mysaadil0@gmail.com>
2023-12-30 14:31:10 +0200

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,6 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """ a function that wites down your first name and last name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
