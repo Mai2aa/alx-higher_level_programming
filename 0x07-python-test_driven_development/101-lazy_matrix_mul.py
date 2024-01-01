3e812496aad8fb664e16f0e773cc91a7b2b23f66
Mai2aa <mysaadil0@gmail.com>
2024-01-01 21:15:47 +0200

    advanced

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -9,9 +9,10 @@ def lazy_matrix_mul(m_a, m_b):
     try:
         result = np.matmul(m_a, m_b)
     except ValueError:
-        raise InvalidMatrixMultiplicationError("m_a and m_b can't be multiplied")
-    
+        raise InvalidMatrixMultiplicationError("m_a and m_b \
+            can't be multiplied")
     return result
+
 
 class InvalidMatrixMultiplicationError(Exception):
     def __init__(self, message):
