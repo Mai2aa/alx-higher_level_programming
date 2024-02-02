e8ac8fd26e45d9bc0e873fd2ad2e82c0772248b1
Mai2aa <mysaadil0@gmail.com>
2024-02-02 03:15:17 +0200

    task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- prints  full description of a table
+SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
+FROM information_schema.columns
+WHERE table_name = 'first_table';
