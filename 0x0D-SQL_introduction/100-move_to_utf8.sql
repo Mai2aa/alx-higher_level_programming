346e629079b611ebc223c80dddf08b7ab4d3b867
Mai2aa <mysaadil0@gmail.com>
2024-02-02 04:19:22 +0200

    advanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,4 @@
 -- Save this SQL script in a file named 'convert_to_utf8.sql'
-
-USE hbtn_0c_0;
-
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 ALTER TABLE first_table MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
