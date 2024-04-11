be4ca08eee3369afd3997f404a0696bfa373edb2
Mai2aa <mysaadil0@gmail.com>
2024-04-11 06:43:58 -0700

    advanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,6 +4,5 @@ CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table
-MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
