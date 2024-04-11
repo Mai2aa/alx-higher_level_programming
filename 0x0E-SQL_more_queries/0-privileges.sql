43750dc2d0ddb2318a3f29cbf1f05a1201204c87
Mai2aa <mysaadil0@gmail.com>
2024-04-11 08:03:47 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
 -- list all the priviliges of mySQL users
+CREATE USER 'user_0d_1'@'%' IDENTIFIED BY '';
+CREATE USER 'user_0d_2'@'%' IDENTIFIED BY '';
 SHOW GRANTS FOR 'user_0d_1'@'%';
 SHOW GRANTS FOR 'user_0d_2'@'%';
