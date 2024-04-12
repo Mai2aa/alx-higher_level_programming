1aa7e2573acb99d2188d6ad5463b8eca47fff651
Mai2aa <mysaadil0@gmail.com>
2024-04-12 19:45:31 +0200

    task 6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create a new database and a new table inside it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT PRIMARY KEY UNIQUE AUTO_INCREMENT NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
