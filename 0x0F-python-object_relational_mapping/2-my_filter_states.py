93cf4356de444a0c6b9e411e09c81f0673b65782
Mai2aa <mysaadil0@gmail.com>
2024-04-13 08:28:58 -0700

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
                            passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     state_name = sys.argv[4]
-    cur.execute("SELECT * FROM states WHERE name='{}'
+    cur.execute("SELECT * FROM states WHERE name=BINARY'{}'\
                 ORDER BY id ASC".format(state_name))
     rows = cur.fetchall()
     for row in rows:
