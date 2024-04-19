f50fb01dd33877c17d263ac6cec43eba34246296
Mai2aa <mysaadil0@gmail.com>
2024-04-19 15:40:39 -0700

    advanced 1

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Script that adds State object “Louisiana” to database hbtn_0e_6_usa"""
+"""
+Script that adds the State object “Louisiana” to the database hbtn_0e_6_usa
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from relationship_city import City
 
-
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
+              .format(argv[0]))
+        exit(1)
 
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
