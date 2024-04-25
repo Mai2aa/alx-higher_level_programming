950fd3d03c2f1636481da67d574cab1bc5a1331f
Mai2aa <mysaadil0@gmail.com>
2024-04-25 14:05:24 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL and displays all HTTP methods the server will accept
-curl -sI "$1" | grep -i '^allow:' | awk '{print $2}'
+curl -sI "$1" | awk '/^Allow:/{gsub("Allow: ",""); print}'
