cba94426d97cefafaf01254bd8198a12ea0e0889
Mai2aa <mysaadil0@gmail.com>
2024-04-25 14:02:03 -0700

    taask 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Sends a GET request to the URL
-curl -s -X GET -w "%{http_code}" "$1" | awk 'BEGIN{body=0} {if(body) print} /^200$/{body=1}'
+curl -s -X GET -w "%{http_code}" "$1" | awk '/^200$/{body=1} body' | tail -n +2
