25d8370031a00f64bd28b91f8237edf408e54f1b
Mai2aa <mysaadil0@gmail.com>
2024-04-26 16:19:12 -0700

    task 7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Error code #1 using requests package"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    num_of_error = response.status_code
+    if num_of_error >= 400:
+        print("Error code: {}".format(num_of_error))
