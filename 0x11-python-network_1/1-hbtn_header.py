3739a69c0af5937b9357ce8a5709ef01039d20e2
Mai2aa <mysaadil0@gmail.com>
2024-04-26 14:52:32 -0700

    task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Response header value #0 """
+import sys
+import urllib.request
+
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    header_id = response.getheader('X-Request-Id')
+
+print(header_id)
