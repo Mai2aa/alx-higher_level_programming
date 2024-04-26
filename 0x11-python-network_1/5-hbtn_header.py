5eccfa9ccdd3b6984b83b1cca27cabd55318b823
Mai2aa <mysaadil0@gmail.com>
2024-04-26 15:56:55 -0700

    task 5 yay

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Response header value #1 """
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    header_id = response.headers['X-Request-Id']
+    print(header_id)
