d2783946f1e4a6b38255b9b2c304c90ae1f0cdc9
Mai2aa <mysaadil0@gmail.com>
2024-04-26 17:41:43 -0700

    advanced last

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,8 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = "https://developer.github.com/v3/repos/{repo_name}/
-           {owner_name}/commits/"
+    url = "https://developer.github.com/v3/repos/{}/{}/commits/".format(
+            repo_name, owner_name)
     response = requests.get(url)
     commits = response.json()
     for commit in commits[:10]:
