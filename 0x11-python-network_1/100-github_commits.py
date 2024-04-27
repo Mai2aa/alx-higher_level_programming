c18c2f3de54c6a11fd9d5ad12f9a9675bfc3a82b
Mai2aa <mysaadil0@gmail.com>
2024-04-26 17:44:58 -0700

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner_name = sys.argv[2]
-    url = "https://developer.github.com/v3/repos/{}/{}/commits/".format(
+    url = "https://developer.github.com/repos/{}/{}/commits/".format(
             repo_name, owner_name)
     response = requests.get(url)
     commits = response.json()
