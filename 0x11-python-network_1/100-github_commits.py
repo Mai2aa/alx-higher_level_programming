168d1f04ee36e3dbcb4b3ce2ee5e98f28bd41f14
Mai2aa <mysaadil0@gmail.com>
2024-04-26 17:34:39 -0700

    last

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     response = requests.get(url)
     commits = response.json()
     for commit in commits[:10]:
-        sha = commit["commit"]["id"]
+        sha = commit["commit"]["sha"]
         auth_name = commit["commit"]["author"]["name"]
         print("{}: {}".format(sha, auth_name))
