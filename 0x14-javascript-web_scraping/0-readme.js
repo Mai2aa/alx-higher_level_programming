746cc5d2542a4b798372c2db9c4e8fdc086643fa
Mai2aa <mysaadil0@gmail.com>
2024-05-21 13:31:11 -0700

    a script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 const fs = require('fs');
-function read_file(file_path) {
-	try {
-		const content = fs.readFileSync(file_path, 'utf8');
-		console.log(content);
-	}
-	catch(error) {
-		console.error(`Error: ${error}`);
-	}
+function readFile (filePath) {
+  try {
+    const content = fs.readFileSync(filePath, 'utf8');
+    console.log(content);
+  } catch (error) {
+    console.error(`Error: ${error}`);
+  }
 }
 if (process.argv.length < 3) {
-	console.log('Usage: node script.js <file_path>');
-}
-else {
-	const file_path = process.argv[2];
-	read_file(file_path);
+  console.log('Usage: node script.js <file_path>');
+} else {
+  const filePath = process.argv[2];
+  readFile(filePath);
 }
